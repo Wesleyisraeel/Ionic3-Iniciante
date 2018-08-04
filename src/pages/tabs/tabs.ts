07f63f9405f9500af0c3af3a9d0ab1f0aef530be
wesleyisraeel <wesley20israel@gmail.com>
2018-08-04 13:42:35 -0300

    commit message

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+//import { AboutPage } from '../about/about';
+//import { ContactPage } from '../contact/contact';
+import { HomePage } from '../home/home';
+import { IntroPage } from '../intro/intro';
+import { FeedPage } from '../feed/feed';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = HomePage;
+  tab2Root = FeedPage;
+  tab3Root = IntroPage;
+  constructor() {
+
+  }
+}
